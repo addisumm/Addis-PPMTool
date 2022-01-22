2ad7bfa2d2f418dd9aa5c954631e78baa2f3f219
User <addisumm@gmail.com>
2022-01-22 14:00:46 -0600

    Branch14: Bringing Bootstrap 5+

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,14 +1,16 @@
 import React,  { Component } from "react";
 import './App.css';
 import Dashboard from './components/Dashboard';
+import Header from "./components/Layout/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 
 class App extends Component{
   render (){
     return (
       <div className='App'>
-      <Dashboard/>
-      <Dashboard/>
-      <Dashboard/>
+      <Header/>
+      <Dashboard/>  
       </div>
     );
   }
--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import ProjectItem from './Project/ProjectItem';
 
   class Dashboard extends Component {
   render() {
     return(
-        <h1>Welcome to the First component</h1>
-    )
+        <div> 
+        <h1 className='alert alert-warning'>Welcome to the Dashboard</h1>
+        <ProjectItem/>
+        <ProjectItem/>
+        <ProjectItem/>
+        </div>
+    );
   }
 }
 
