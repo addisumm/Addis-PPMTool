8ca88fb677c050b7a6434e2452e63198a3727eb5
User <addisumm@gmail.com>
2022-01-28 22:39:58 -0600

    Branch32: Backlog - projectTask relationship

--- PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/domain/Backlog.java
+++ PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/domain/Backlog.java
@@ -1,9 +1,9 @@
 package io.agileintelligence.ppmtool.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
 public class Backlog {
@@ -13,7 +13,12 @@ public class Backlog {
     private Integer PTSeqquence =0;
     private String projectIdentifier;
     //OneToOne with project
+    @OneToOne (fetch = FetchType.EAGER)
+    @JoinColumn(name = "project_id", nullable = false)
+    @JsonIgnore
+    private Project project;
     //OneToMany projecttasks
+
 
 
     public Backlog () {
@@ -41,5 +46,13 @@ public class Backlog {
 
     public void setProjectIdentifier (String projectIdentifier) {
         this.projectIdentifier = projectIdentifier;
+    }
+
+    public Project getProject () {
+        return project;
+    }
+
+    public void setProject (Project project) {
+        this.project = project;
     }
 }
