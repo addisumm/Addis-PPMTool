bc962efe99e3b18f049fec119b9269aad6c59ac7
User <addisumm@gmail.com>
2022-01-30 07:26:38 -0600

    Find projectTask by ProjectSequence w/Validation

--- PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/web/ProjectController.java
+++ PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/web/ProjectController.java
@@ -1,8 +1,10 @@
 package io.agileintelligence.ppmtool.web;
 
 import io.agileintelligence.ppmtool.domain.Project;
+import io.agileintelligence.ppmtool.domain.ProjectTask;
 import io.agileintelligence.ppmtool.services.MapValidationErrorService;
 import io.agileintelligence.ppmtool.services.ProjectService;
+import io.agileintelligence.ppmtool.services.ProjectTaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +23,9 @@ public class ProjectController {
 
     @Autowired
     private MapValidationErrorService mapValidationErrorService;
+
+    @Autowired
+    private ProjectTaskService projectTaskService;
 
     @PostMapping
     public ResponseEntity <?> createNewProject (@Valid @RequestBody Project project, BindingResult result) {
@@ -45,5 +50,10 @@ public class ProjectController {
     public ResponseEntity <?> deleteProject (@PathVariable String projectId) {
         projectService.deleteProjectByIdentifier (projectId);
         return new ResponseEntity <String> ("Project with Id ' " + projectId + " 'was deleted ", HttpStatus.OK);
+    }
+    @GetMapping("{backlog_id}/{pt_id}")
+    public ResponseEntity<?> getProjectTask(@PathVariable String backlog_id, @PathVariable String pt_id){
+        ProjectTask projectTask=projectTaskService.findPTByProjectSequence (backlog_id,pt_id);
+        return  new ResponseEntity <ProjectTask> (projectTask, HttpStatus.OK);
     }
 }
