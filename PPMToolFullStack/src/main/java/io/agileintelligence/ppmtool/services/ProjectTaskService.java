7b9f15ec2dc48ae9ea695716aef94ae393ac5d56
User <addisumm@gmail.com>
2022-01-30 20:39:50 -0600

    Branch42: Bug fix, Delete operation, improved backlog/project task

--- PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/services/ProjectTaskService.java
+++ PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/services/ProjectTaskService.java
@@ -90,18 +90,8 @@ public class ProjectTaskService {
 
     public void deleteByProjectSequence(String backlog_id, String pt_id){
         ProjectTask projectTask =findPTByProjectSequence(backlog_id, pt_id);
-
-        Backlog backlog = projectTask.getBacklog ();
-        List<ProjectTask> pts= backlog.getProjectTasks ();
-        pts.remove (projectTask);
-        backlogRepository.save (backlog);
         projectTaskRepository.delete (projectTask);
     }
-
-    //Update projectTask
-    //find existing projectTask
-    //replace with update task
-    //save update
 
 
 }
